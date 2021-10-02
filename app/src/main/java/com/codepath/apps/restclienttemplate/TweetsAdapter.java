5cba3704ca9e07ace82230e89b5a474feb24cadf
mohamedkharma <mohamedkharma@hotmail.com>
2021-10-01 21:28:45 -0400

    added the loadMoreData

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -13,20 +13,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder>{
-
     Context context;
     List<Tweet> tweets;
-    //Pass in the context and list of tweets through a constructor
-    public TweetsAdapter(Context context, List<Tweet> tweets){
+
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
         this.context = context;
         this.tweets = tweets;
     }
 
-    //For each row, inflate the layout
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -34,12 +31,9 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         return new ViewHolder(view);
     }
 
-    //Bind values based on the position of the element
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        //Get the data at position
         Tweet tweet = tweets.get(position);
-        //Bind the tweet with the view holder
         holder.bind(tweet);
     }
 
@@ -48,41 +42,40 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         return tweets.size();
     }
 
-     //Clean all elements of the recycler
-    public void clear() {
-        tweets = new ArrayList<>();
+    public void clear(){
         tweets.clear();
         notifyDataSetChanged();
     }
 
-    // Add a list of items -- change to type used
-    public void addAll(List<Tweet> tweetList) {
+    public void addAll(List<Tweet> tweetList){
         tweets.addAll(tweetList);
         notifyDataSetChanged();
     }
 
-    //Define a Viewholder
-    public class ViewHolder extends RecyclerView.ViewHolder{
 
+    public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView ivProfileImage;
         TextView tvBody;
         TextView tvScreenName;
-//        TextView tvTimeStamp;
+        TextView tvTime;
+        TextView tvName;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder (@NonNull View itemView){
             super(itemView);
             ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
             tvBody = itemView.findViewById(R.id.tvBody);
+            tvTime = itemView.findViewById(R.id.tvTime);
             tvScreenName = itemView.findViewById(R.id.tvScreenName);
-//            tvTimeStamp = itemView.findViewById(R.id.tvTimestamp);
+            tvName = itemView.findViewById(R.id.tvName);
         }
 
         public void bind(Tweet tweet) {
-//            tvTimeStamp.setText(tweet.getFormattedTimestamp());
             tvBody.setText(tweet.body);
-            tvScreenName.setText(tweet.user.screenName);
+            tvScreenName.setText('@'+tweet.user.screenName);
+            tvTime.setText(tweet.getFormattedTimestamp());
+            tvName.setText(tweet.user.name);
+
             Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
         }
     }
 }
-
--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -10,19 +10,19 @@ import com.github.scribejava.apis.TwitterApi;
 import com.github.scribejava.core.builder.api.BaseApi;
 
 /*
- * 
- * This is the object responsible for communicating with a REST API. 
+ *
+ * This is the object responsible for communicating with a REST API.
  * Specify the constants below to change the API being communicated with.
- * See a full list of supported API classes: 
+ * See a full list of supported API classes:
  *   https://github.com/scribejava/scribejava/tree/master/scribejava-apis/src/main/java/com/github/scribejava/apis
  * Key and Secret are provided by the developer site for the given API i.e dev.twitter.com
  * Add methods for each relevant endpoint in the API.
- * 
+ *
  * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
- * 
+ *
  */
 public class TwitterClient extends OAuthBaseClient {
-	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance();
+	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance(); // Change this
 	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
 	public static final String REST_CONSUMER_KEY = BuildConfig.CONSUMER_KEY;       // Change this inside apikey.properties
 	public static final String REST_CONSUMER_SECRET = BuildConfig.CONSUMER_SECRET; // Change this inside apikey.properties
@@ -44,21 +44,21 @@ public class TwitterClient extends OAuthBaseClient {
 	}
 	// CHANGE THIS
 	// DEFINE METHODS for different API endpoints here
-	public void getHomeTimeLine(JsonHttpResponseHandler handler) {
+	public void getHomeTimeline(JsonHttpResponseHandler handler) {
 		String apiUrl = getApiUrl("statuses/home_timeline.json");
 		// Can specify query string params directly or through RequestParams.
 		RequestParams params = new RequestParams();
 		params.put("count", 25);
-		params.put("since_id", 1);
+		params.put("since_id",1);
 		client.get(apiUrl, params, handler);
 	}
 
-	public void getNextPageOfTweets(JsonHttpResponseHandler handler, long maxId) {
+	public void getNextPageOfTweets(JsonHttpResponseHandler handler, Long maxId) {
 		String apiUrl = getApiUrl("statuses/home_timeline.json");
 		// Can specify query string params directly or through RequestParams.
 		RequestParams params = new RequestParams();
 		params.put("count", 25);
-		params.put("max_id", maxId);
+		params.put("max_Id",maxId);
 		client.get(apiUrl, params, handler);
 	}
 
